b8b1f2f07b004ebaecdd9cedbc5ae5744a8b0e68
Faaiz Ahmad <faizahmad6420@gmail.com>
2024-07-26 17:52:29 +0500

    Add authentication and integrate Bootstrap

--- Gemfile
+++ Gemfile
@@ -65,3 +65,9 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "devise", "~> 4.9"
+
+gem 'cssbundling-rails'
+
+gem 'devise-bootstrap-views', '~> 1.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_060537) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_071750) do
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password"
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_060537) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
