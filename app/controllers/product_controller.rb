6f8610a6fcc95c331730db50d9121a3d6abfe53c
Faaiz Ahmad <faizahmad6420@gmail.com>
2024-08-13 10:31:37 +0500

    Correct Logic (Add quantity if product already exist in cart)

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -27,7 +27,13 @@ class ProductController < ApplicationController
           product: product,
           size_id: size_id
         )
-        cart_item.quantity = quantity
+
+        if cart_item.persisted?
+          cart_item.quantity += quantity
+        else
+          cart_item.quantity = quantity
+        end
+        
         cart_item.price = product.price
         cart_item.save
         redirect_to cart_path, notice: 'Product added to cart'
