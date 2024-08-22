621d4b0555aab051a2b39629c028a88369533287
Faaiz Ahmad <faizahmad6420@gmail.com>
2024-08-22 15:55:18 +0500

    Set Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,31 +4,24 @@ Rails.application.routes.draw do
 
   devise_for :customers
   resources :customers, only: [:index]
-  resources :product, only: [:show]
 
-  resources :product do
+  resources :product, only: [:show]do
     post 'add_to_cart', on: :member
   end
 
-  resources :carts, only: [:show]
-
-  resources :carts do
+  resources :carts, only: [:show] do
     member do
       delete :remove_from_cart
     end
   end
 
-  resources :orders, only: [:show] do
+  resources :orders, only: [:index, :show] do
+    resources :payments, only: [:new, :create]
     member do
       post :checkout
     end
   end
 
-  resources :orders do
-    resources :payments, only: [:new, :create]
-  end
-  
-  get 'orders', to: 'orders#index', as: 'customer_orders'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
